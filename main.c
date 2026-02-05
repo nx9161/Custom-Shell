ec7195afd901a00c100472367b567dbf671a9316
nx9161 <naman.9161@icloud.com>
2026-02-05 00:15:59 -0500

    Added the main source code

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // Includes standard input/output library
+
+int main() {
+    // This is the starting point
+    printf("Hello, World!"); 
+    return 0; // Tells the OS the program finished successfully
+}
